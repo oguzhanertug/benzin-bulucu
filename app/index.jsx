99dc184e923960f9cac80f38b02530c687607a68
oguzhanertug <oguzhannertug@gmail.com>
2026-06-03 00:45:59 +0300

    Module 7: istasyon listesi ve en ucuz bulma eklendi

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,57 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { StyleSheet, Text, View, Pressable, FlatList } from 'react-native'
 import React from 'react'
-
+const istasyonlar = [
+  {
+    isim: "A istasyonu",
+    adres: "x mahallesi",
+    fiyat: 40,
+    konum: { lat: 40.323, lng: 41.200 }
+  },
+  {
+    isim: "B istasyonu",
+    adres: "y mahallesi",
+    fiyat: 39,
+    konum: { lat: 41.500, lng: 42.300 }
+  },
+  {
+    isim: "C istasyonu",
+    adres: "z mahallesi",
+    fiyat: 38,
+    konum: { lat: 43.200, lng: 44.500 }
+  },
+]
 const Home = () => {
+  let enUcuz = istasyonlar[0]
+
+for (let i = 0; i < istasyonlar.length; i++) {
+  if (istasyonlar[i].fiyat < enUcuz.fiyat) {
+    enUcuz = istasyonlar[i]
+  }
+}
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Benzin Bulucu</Text>
       <Text style={styles.subtitle}>En ucuz istasyonu hemen bul</Text>
+      <Text style={styles.enUcuzBaslik}>🏆 En Ucuz İstasyon</Text>
+<View style={styles.enUcuzKart}>
+  <Text style={styles.kartIsim}>{enUcuz.isim}</Text>
+  <Text>{enUcuz.adres}</Text>
+  <Text>⛽ {enUcuz.fiyat} TL</Text>
+</View>
       <Pressable style={styles.button}>
         <Text style={styles.buttonText}>Başla</Text>
       </Pressable>
+      <FlatList
+  data={istasyonlar}
+  keyExtractor={(item) => item.isim}
+  renderItem={({ item }) => (
+    <View style={styles.kart}>
+      <Text style={styles.kartIsim}>{item.isim}</Text>
+      <Text>{item.adres}</Text>
+      <Text>⛽ {item.fiyat} TL</Text>
+    </View>
+  )}
+/>
     </View>
   )
 }
@@ -43,4 +86,29 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  kart: {
+  backgroundColor: '#f5f5f5',
+  padding: 15,
+  marginVertical: 8,
+  borderRadius: 10,
+  width: 300,
+},
+kartIsim: {
+  fontSize: 18,
+  fontWeight: 'bold',
+  marginBottom: 5,
+},
+enUcuzBaslik: {
+  fontSize: 20,
+  fontWeight: 'bold',
+  color: '#2ecc71',
+  marginBottom: 8,
+},
+enUcuzKart: {
+  backgroundColor: '#d5f5e3',
+  padding: 15,
+  borderRadius: 10,
+  width: 300,
+  marginBottom: 20,
+},
 })
